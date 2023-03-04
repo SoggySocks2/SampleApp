3886c2b2eab0c22938c2c681a6646e45635125c8
SoggySocks <peter_jones_glass@hotmail.com>
2023-03-04 09:14:46 +0000

    Add InvalidFormatException test

---
+++ SampleApp.Tests/Unit Tests/InvalidFormatExceptionTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using SampleApp.Library;
+
+namespace SampleApp.Tests.Unit_Tests
+{
+    public class InvalidFormatExceptionTests
+    {
+        [Fact]
+        public void InvalidFormatException_ShouldHave_CorrectMessage()
+        {
+            var ex = new InvalidFormatException();
+            ex.Should().BeOfType<InvalidFormatException>();
+            ex.Message.Should().Be("String was in an invalid format");
+        }
+
+    }
+}
